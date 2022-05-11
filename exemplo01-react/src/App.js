115dde799a1e03d59ae31569ef6b38e3746a6327
GabrielCalazans180 <77144606+GabrielCalazans180@users.noreply.github.com>
2022-05-11 16:54:44 -0300

    Add files via upload

---
+++ exemplo01-react/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Formulario from "./Components/Formulario";
+import Lista from "./Components/Lista";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Formulario />
+      <Lista />
+    </div>
+  );
+}
