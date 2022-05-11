ae1c4ef8b42cfabef62256226d3ba61566e454dc
GabrielCalazans180 <77144606+GabrielCalazans180@users.noreply.github.com>
2022-05-11 17:01:39 -0300

    Delete src directory

--- src/Components/Botao/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Botao extends React.Component<{ texto: string }> {
-  render() {
-    return <button>{this.props.texto}</button>;
-  }
-}
-
-export default Botao;
